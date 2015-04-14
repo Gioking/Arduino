1068f803476f3fc5d84c5fb43a993594863d559a
gioking <gioking@gmail.com>
2015-04-14 21:39:13 +0200

    First Commit

---
+++ MvcEF/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcEF.Startup))]
+namespace MvcEF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
